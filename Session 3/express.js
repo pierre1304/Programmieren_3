679f2f1d0345c4360e9e5860da5064035c461368
Pierre Raffel <pierre.raffel@gmail.com>
2022-10-07 20:06:33 +0200

    Session 3

---
+++ Session 3/express.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello world!");
+});
+
+app.get("/name/:name", function(req, res){
+   let name = req.params.name;
+    res.send("<h1>Hello " + name + "</h1>");
+});
+
+app.get("/google", function(req, res){
+    res.redirect("http://google.de")
+}) 
+
+app.listen(3000, function () {
+    console.log("hallo");
+});
---
+++ Session 3/index.js
@@ -0,0 +1,9 @@
+const square = require("./module");
+
+function main() {
+    console.log(mySquareObject.getArea());
+    let mySquareObject = new Square(5);
+    console.log("Flaeche: ", mySquareObject.getArea());
+}
+
+main();
